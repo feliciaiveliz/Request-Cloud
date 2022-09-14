1256ff25b408a462ff2be0e3420632f370fcde2c
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-09-14 17:11:23 -0500

    extract http request/response details from getBins database query function

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const helper = require('./utility/helper')
+const { response } = require('express')
 
 const Pool = require('pg').Pool
 const pool = new Pool({
@@ -12,30 +13,31 @@ const pool = new Pool({
   port: 5432,
 })
 
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM bins', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+const getBins = async () => {
+  try {
+    const result = await pool.query('SELECT * FROM bins')
+    return result.rows
+  } catch (error) {
+    console.log("ERROR accessing all bins. Stack:")
+    console.log(error.stack)
+  }
 }
 
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  getUsers(req, res)
+
+app.get('/', async (_, response) => {
+  try {
+    const bins = await getBins()
+    response.status(200).json(bins)
+  } catch(error) {
+    console.log("oops");
+    response.status(404).end();
+  }
 })
 
 // app.get('/api/bin/:id', (req, res) => {
 
 // })
-
-app.post('/api/bin', (req, res) => {
-  const path = helper.random(20);
-  
-
-  res.send(path)
-})
 
 app.listen(port, () => {
   console.log(`start of our Request Box backend on port ${port}`)
