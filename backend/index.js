32636c6eefade0d70e946b2f3e00278414db4ce2
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-09-14 17:40:41 -0500

    Create createBin function to add new bins and POST /api/bins to create new bins

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
-const helper = require('./utility/helper')
-const { response } = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const helper = require('./utility/helper');
+const { response } = require('express');
 
 const Pool = require('pg').Pool
 const pool = new Pool({
@@ -11,7 +11,7 @@ const pool = new Pool({
   user: 'ben',
   database: 'request_box',
   port: 5432,
-})
+});
 
 const getBins = async () => {
   try {
@@ -23,7 +23,7 @@ const getBins = async () => {
   }
 }
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/api/bins', async (_, response) => {
   try {
@@ -36,24 +36,29 @@ app.get('/api/bins', async (_, response) => {
   }
 });
 
-// app.get('/api/bin/:id', (req, res) => {
+const createBin = async (path) => {
+  const text = "INSERT INTO bins (path) VALUES ($1)";
 
-// })
+  try {
+    await pool.query(text, [path]);
+  } catch (error) {
+    console.log("ERROR accessing all bins. Stack:");
+    console.log(error.stack);
+  }
+}
+
+app.post('/api/bins', async (_, response) => {
+  const path = helper.random(20);
+
+  try {
+    await createBin(path)
+    console.log("IS THIS WOKRING?");
+    response.status(201).send(path);
+  } catch (error) {
+    response.status(409).end();
+  }
+});
 
 app.listen(port, () => {
   console.log(`start of our Request Box backend on port ${port}`)
 })
-
-/*
-
-creating a bin
-  POST
-  generate unique path
-  create a post request
-    - connect to postgres db => done
-    -  
-
-  
-
-
-*/
