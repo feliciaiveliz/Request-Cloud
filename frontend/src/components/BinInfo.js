09542ec5e9b6b1c4a1a0662b6f5b64030674900d
Kim Ramirez <kim.ram27@gmail.com>
2022-09-16 08:17:59 -0600

    change url

--- frontend/src/components/BinInfo.js
+++ frontend/src/components/BinInfo.js
@@ -2,7 +2,7 @@ const BinInfo = ({ info }) => {
   return (
     <div className="binInfo">
       <p>HTTP</p>
-      <p>Url: {`https://localhost:3000/${info.path}`}</p>
+      <p>Url: {`https://api.mattmalane.dev/target/${info.path}`}</p>
       <p>Date Created: {info["date_created"]}</p>
       <p>Date Last Updated: {info["date_last_updated"]}</p>
     </div>
